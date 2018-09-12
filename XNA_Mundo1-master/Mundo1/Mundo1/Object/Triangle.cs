abde652a31a4c78392aec0e23f3970b3d2a420ae
fellipe eduardo <fellipeedu@LIVE.COM>
2018-09-12 19:14:15 -0300

    Mundo 1 Finalizado (sem ajustes)

---
+++ XNA_Mundo1-master/Mundo1/Mundo1/Object/Triangle.cs
@@ -0,0 +1,56 @@
+﻿#region using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Mundo1.Set;
+#endregion
+
+namespace Mundo1.Object
+{
+    class Triangle
+    {
+        protected GraphicsDevice device;
+        protected Matrix world;
+        VertexPositionColor[] verts = new VertexPositionColor[3];
+        VertexBuffer vertexBuffer;
+        BasicEffect effect;
+
+        public Triangle(GraphicsDevice device, Vector3 p1, Vector3 p2, Vector3 p3, Color color)
+        {
+            this.device = device;
+            world = Matrix.Identity;
+
+            verts[0] = new VertexPositionColor(p1, color);
+            verts[1] = new VertexPositionColor(p2, color);
+            verts[2] = new VertexPositionColor(p3, color);
+
+            vertexBuffer = new VertexBuffer(device, typeof(VertexPositionColor), verts.Length, BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(verts);
+
+            effect = new BasicEffect(this.device);
+        }
+
+        public virtual void Draw(Camera camera)
+        {
+            device.SetVertexBuffer(this.vertexBuffer);
+
+            effect.World = world;
+            effect.View = camera.GetView();
+            effect.Projection = camera.GetProjection();
+            effect.VertexColorEnabled = true;
+
+            foreach (EffectPass i in this.effect.CurrentTechnique.Passes)
+            {
+                i.Apply();
+                this.device.DrawUserPrimitives<VertexPositionColor>(PrimitiveType.TriangleStrip, verts, 0, 1);
+            }
+        }
+    }
+}
---
+++ XNA_Mundo1-master/Mundo1/Mundo1/Set/Scene.cs
@@ -0,0 +1,427 @@
+﻿#region using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Mundo1.Object;
+#endregion
+
+namespace Mundo1.Set
+{
+    public static class Scene
+    {
+        private static List<Triangle> triangles;
+
+        static Telao telao;
+        static Camera camera;
+        static GraphicsDevice devicE;
+
+        public static void Initialize(GraphicsDevice device, int screenWidth, int screeHeight)
+        {
+            devicE = device;
+
+            telao = Telao.GetInstance();
+            telao.SetWidth(screenWidth);
+            telao.SetHeight(screeHeight);
+
+            camera = new Camera();
+
+            triangles = new List<Triangle>();
+
+            #region Parede Frontal
+            Color paredeFrontal = Color.Red;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, 1.5f, -3.75f),
+                        new Vector3(1.5f, 1.5f, -3.75f),
+                        new Vector3(-1.5f, 0.5f, -3.75f),
+                        paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 1.5f, -3.75f),
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(0.65f, 0.5f, -3.75f),
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(0.65f, -1.5f, -3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(1.5f, -1.5f, -3.75f),
+                    new Vector3(0.65f, -1.5f, -3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    new Vector3(-0.65f, 0.5f, -3.75f),
+                    new Vector3(-0.65f, -1.5f, -3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    new Vector3(-0.65f, -1.5f, -3.75f),
+                    new Vector3(-1.5f, -1.5f, -3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, 1.5f, 3.75f),
+                    new Vector3(1.5f, 1.5f, 3.75f),
+                    new Vector3(-1.5f, -1.5f, 3.75f),
+                    paredeFrontal));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, 3.75f),
+                    new Vector3(1.5f, 1.5f, 3.75f),
+                    new Vector3(1.5f, -1.5f, 3.75f),
+                    paredeFrontal));
+            #endregion
+
+            #region Parede direita
+            Color paredeDireita = Color.Blue;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, 1.5f, 3.75f),
+                        new Vector3(-1.5f, 1.5f, -3.75f),
+                        new Vector3(-1.5f, 0.5f, -3.75f),
+                        paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, 1.5f, 3.75f),
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    new Vector3(-1.5f, 0.5f, 3.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -0.2f, 1.1f),
+                    new Vector3(-1.5f, 0.5f, 1.1f),
+                    new Vector3(-1.5f, 0.5f, -0.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -0.2f, 1.1f),
+                    new Vector3(-1.5f, 0.5f, -0.75f),
+                    new Vector3(-1.5f, -0.2f, -0.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, 3.75f),
+                    new Vector3(-1.5f, -0.2f, 3.75f),
+                    new Vector3(-1.5f, -0.2f, -0.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, 3.75f),
+                    new Vector3(-1.5f, 0.2f, -0.75f),
+                    new Vector3(-1.5f, -1.5f, -0.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -0.2f, 3.75f),
+                    new Vector3(-1.5f, 0.5f, 3.75f),
+                    new Vector3(-1.5f, 0.5f, 1.8f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -0.2f, 3.75f),
+                    new Vector3(-1.5f, 0.5f, 1.8f),
+                    new Vector3(-1.5f, -0.2f, 1.8f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, -2.75f),
+                    new Vector3(-1.5f, 0.5f, -2.75f),
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, -2.75f),
+                    new Vector3(-1.5f, 0.5f, -3.75f),
+                    new Vector3(-1.5f, -1.5f, -3.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, -0.75f),
+                    new Vector3(-1.5f, -0.5f, -0.75f),
+                    new Vector3(-1.5f, -0.5f, -2.75f),
+                    paredeDireita));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, -1.5f, -0.75f),
+                    new Vector3(-1.5f, -0.5f, -2.75f),
+                    new Vector3(-1.5f, -1.5f, -2.75f),
+                    paredeDireita));
+
+            #endregion
+
+            #region Parede esquerda
+            Color paredeEsquerda = Color.Brown;
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 1.5f, -3.75f),
+                    new Vector3(1.5f, 1.5f, 3.75f),
+                    new Vector3(1.5f, 0.5f, 3.75f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(1.5f, 1.5f, -3.75f),
+                    new Vector3(1.5f, 0.5f, 3.75f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.3f, -1.4f),
+                    new Vector3(1.5f, 0.5f, -1.4f),
+                    new Vector3(1.5f, 0.5f, 1.1f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.3f, -1.4f),
+                    new Vector3(1.5f, 0.5f, 1.1f),
+                    new Vector3(1.5f, 0.3f, 1.1f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, -1.5f, -2.1f),
+                    new Vector3(1.5f, -0.2f, -2.1f),
+                    new Vector3(1.5f, -0.2f, -1.4f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, -1.5f, -2.1f),
+                    new Vector3(1.5f, -0.2f, -1.4f),
+                    new Vector3(1.5f, -1.5f, -1.4f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.3f, -1.4f),
+                    new Vector3(1.5f, 0.3f, 1.8f),
+                    new Vector3(1.5f, -1.5f, 1.8f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.3f, -1.4f),
+                    new Vector3(1.5f, -1.5f, 1.8f),
+                    new Vector3(1.5f, -1.5f, -1.4f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.5f, 1.8f),
+                    new Vector3(1.5f, -1.5f, 1.8f),
+                    new Vector3(1.5f, -1.5f, 3.75f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.5f, 1.8f),
+                    new Vector3(1.5f, 0.5f, 3.75f),
+                    new Vector3(1.5f, -1.5f, 3.75f),
+                    paredeEsquerda));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(1.5f, 0.5f, -2.1f),
+                    new Vector3(1.5f, -1.5f, -2.1f),
+                    paredeEsquerda)); 
+            
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(1.5f, -1.5f, -3.75f),
+                    new Vector3(1.5f, 0.5f, -3.75f),
+                    new Vector3(1.5f, -1.5f, -2.1f),
+                    paredeEsquerda)); 
+
+            #endregion
+
+            #region Teto
+            Color teto = Color.Brown;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, 1.5f, 3.75f),
+                        new Vector3(1.5f, 1.5f, 3.75f),
+                        new Vector3(1.5f, 1.5f, -3.75f),
+                        teto));
+
+            triangles.Add(
+                new Triangle(devicE,
+                    new Vector3(-1.5f, 1.5f, 3.75f),
+                    new Vector3(1.5f, 1.5f, -3.75f),
+                    new Vector3(-1.5f, 1.5f, -3.75f),
+                    teto)); 
+            #endregion
+
+            #region Chao
+            Color chao = Color.Black;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-8.5f, -1.5f, -9),
+                        new Vector3(8.5f, -1.5f, -9),
+                        new Vector3(8.5f, -1.5f, 9),
+                        chao));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-8.5f, -1.5f, -9),
+                        new Vector3(-8.5f, -1.5f, 9),
+                        new Vector3(8.5f, -1.5f, 9),
+                        chao)); 
+            #endregion
+
+            #region porta
+            Color porta = Color.BlueViolet;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-0.65f, -1.5f, -3.75f),
+                        new Vector3(-0.65f, 0.5f, -3.75f),
+                        new Vector3(0.65f, 0.5f, -3.75f),
+                        porta));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-0.65f, -1.5f, -3.75f),
+                        new Vector3(0.65f, -1.5f, -3.75f),
+                        new Vector3(0.65f, 0.5f, -3.75f),
+                        porta));
+
+            #endregion
+
+            #region janel 2 bandas
+            Color janelaBanda = Color.Salmon;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.5f, -2.75f),
+                        new Vector3(-1.5f, 0.5f, -2.75f),
+                        new Vector3(-1.5f, 0.5f, -1.75f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.5f, -2.75f),
+                        new Vector3(-1.5f, -0.5f, -1.75f),
+                        new Vector3(-1.5f, 0.5f, -1.75f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.5f, -1.75f),
+                        new Vector3(-1.5f, 0.5f, -1.75f),
+                        new Vector3(-1.5f, 0.5f, -0.75f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.5f, -1.75f),
+                        new Vector3(-1.5f, -0.5f,-0.75f),
+                        new Vector3(-1.5f, 0.5f, -0.75f),
+                        janelaBanda));
+
+            #endregion
+
+            #region Janela q sobe
+            Color janelaSobe = Color.RoyalBlue;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.2f, 1.1f),
+                        new Vector3(-1.5f, 0.5f, 1.1f),
+                        new Vector3(-1.5f, 0.5f, 1.8f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(-1.5f, -0.2f, 1.1f),
+                        new Vector3(-1.5f, -0.2f, 1.8f),
+                        new Vector3(-1.5f, 0.5f, 1.8f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(1.5f, 0.5f, -2.1f),
+                        new Vector3(1.5f, -0.2f, -2.1f),
+                        new Vector3(1.5f, -0.2f, -1.4f),
+                        janelaBanda));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(1.5f, 0.5f, -2.1f),
+                        new Vector3(1.5f, 0.5f, -1.4f),
+                        new Vector3(1.5f, -0.2f, -1.4f),
+                        janelaBanda));
+            #endregion
+
+            #region Basculante
+            Color basculante = Color.SeaGreen;
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(1.5f, .3f, 1.1f),
+                        new Vector3(1.5f, 0.5f, 1.1f),
+                        new Vector3(1.5f, 0.5f, 1.8f),
+                        basculante));
+
+            triangles.Add(
+                    new Triangle(devicE,
+                        new Vector3(1.5f, .3f, 1.1f),
+                        new Vector3(1.5f, 0.3f, 1.8f),
+                        new Vector3(1.5f, 0.5f, 1.8f),
+                        basculante));
+            #endregion
+        }
+
+        public static void Draw()
+        {
+            RasterizerState raster = new RasterizerState();
+            raster.CullMode = CullMode.None;
+            //raster.FillMode = FillMode.WireFrame;
+            devicE.RasterizerState = raster;
+
+            foreach (Triangle i in triangles)
+            {
+                i.Draw(camera);
+            }
+        }
+    }
+}
